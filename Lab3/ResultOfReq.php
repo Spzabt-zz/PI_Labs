bcdd3a10b4cf5c807d509a2b7dff446ae5de8bbe
Bohdan <stoida146@gmail.com>
2022-12-05 14:55:40 +0200

    lab3

--- Lab3/ResultOfReq.php
+++ Lab3/ResultOfReq.php
@@ -5,15 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        table, td, th {
-            table-layout: fixed;
-            width: auto;
-            border-collapse: collapse;
-            border: 3px solid black;
-            text-align: left;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css"/>
     <title>Lab3</title>
 </head>
 <body>
