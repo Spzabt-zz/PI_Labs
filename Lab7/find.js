044beed1623bf0d4ee56e24001c0f3f50cf66169
Bohdan <stoida146@gmail.com>
2022-12-07 16:45:15 +0200

    l7

--- Lab7/find.js
+++ Lab7/find.js
@@ -4,13 +4,13 @@ function find(e) {
 
     xhr.open("GET", "./getSearch.php?q=" + q, true);
     xhr.onreadystatechange = () => {
-        if (this.readyState === XMLHttpRequest.DONE && xhr.status === 0
-            || (xhr.status >= 200 && xhr.status < 400)) {
-            document.getElementById("output").innerHTML = xhr.responseText;
-            console.log(xhr.responseText);
-        } else {
+        /*if (this.readyState === XMLHttpRequest.DONE && xhr.status === 0
+            || (xhr.status >= 200 && xhr.status < 400)) {*/
+        document.getElementById("output").innerHTML = xhr.responseText;
+        console.log(xhr.responseText);
+        /*} else {
             console.log("Error!");
-        }
+        }*/
     };
     xhr.send();
 }
