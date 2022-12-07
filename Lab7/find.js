19ec4606b50c70e63198f6e88650a7d0e9c227a0
Bohdan <stoida146@gmail.com>
2022-12-07 17:25:54 +0200

    l7

--- Lab7/find.js
+++ Lab7/find.js
@@ -2,15 +2,16 @@ function find(e) {
     const q = e.target.value;
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "./getSearch.php?q=" + q, true);
+    //xhr.open("GET", "./getSearch.php?q=" + q, true);
+    xhr.open("GET", "https://stoida.herokuapp.com/Lab7/getSearch.php?q=" + q, true);
     xhr.onreadystatechange = () => {
-        /*if (this.readyState === XMLHttpRequest.DONE && xhr.status === 0
-            || (xhr.status >= 200 && xhr.status < 400)) {*/
-        document.getElementById("output").innerHTML = xhr.responseText;
-        console.log(xhr.responseText);
-        /*} else {
+        if (this.readyState === XMLHttpRequest.DONE && xhr.status === 0
+            || (xhr.status >= 200 && xhr.status < 400)) {
+            document.getElementById("output").innerHTML = xhr.responseText;
+            console.log(xhr.responseText);
+        } else {
             console.log("Error!");
-        }*/
+        }
     };
     xhr.send();
 }
