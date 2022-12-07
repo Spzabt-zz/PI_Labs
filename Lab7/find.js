2cc90c979186630d7007fd69d0bd01005565473e
Bohdan <stoida146@gmail.com>
2022-12-07 16:21:00 +0200

    l7

--- Lab7/find.js
+++ Lab7/find.js
@@ -2,7 +2,7 @@ function find(e) {
     const q = e.target.value;
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "getSearch.php?q=" + q, true);
+    xhr.open("GET", "./getSearch.php?q=" + q, true);
     xhr.onreadystatechange = () => {
         if (this.readyState === XMLHttpRequest.DONE && xhr.status === 0
             || (xhr.status >= 200 && xhr.status < 400)) {
