a0f7e58416b430a5c11696221f65ef56dd2c7b7d
Bohdan <stoida146@gmail.com>
2022-12-07 17:28:23 +0200

    l7

--- Lab7/find.js
+++ Lab7/find.js
@@ -2,8 +2,8 @@ function find(e) {
     const q = e.target.value;
     const xhr = new XMLHttpRequest();
 
-    //xhr.open("GET", "./getSearch.php?q=" + q, true);
-    xhr.open("GET", "https://stoida.herokuapp.com/Lab7/getSearch.php?q=" + q, true);
+    xhr.open("GET", "./getSearch.php?q=" + q, true);
+    //xhr.open("GET", "https://stoida.herokuapp.com/Lab7/getSearch.php?q=" + q, true);
     xhr.onreadystatechange = () => {
         if (this.readyState === XMLHttpRequest.DONE && xhr.status === 0
             || (xhr.status >= 200 && xhr.status < 400)) {
